2f70d9cac58c9d4ae16962035ab63dd2925ca554
Asafe Silva <asafemusic15@gmail.com>
2017-06-09 22:55:08 -0300

    Blink_TMR0

--- PIC16F628A/Blink_TMR0/nbproject/Package-default.bash
+++ PIC16F628A/Blink_TMR0/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Blink_TMR0_PIC16F628A.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Blink_TMR0_PIC16F628A.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=blinktmr0pic16f628a.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Blink_TMR0.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=Blink_TMR0.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=blinktmr0/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/blinktmr0pic16f628a.x/bin
+makeDirectory ${TMPDIR}/blinktmr0/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/blinktmr0pic16f628a.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/blinktmr0.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/blinktmr0pic16f628a.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/blinktmr0.tar *
 checkReturnCode
 
 # Cleanup
